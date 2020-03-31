59e183666f2d1b5278b03560f7b77c0cb4746121
Diego Chavez <diegochavez@diegochavez.local>
2020-03-31 19:06:48 -0300

    commit inicial

---
+++ src/main/java/com/dchavez/amqp/client/RpcRabbitmqClientApplication.java
@@ -0,0 +1,13 @@
+package com.dchavez.amqp.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcRabbitmqClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpcRabbitmqClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dchavez/amqp/client/RpcRabbitmqClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dchavez.amqp.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcRabbitmqClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
