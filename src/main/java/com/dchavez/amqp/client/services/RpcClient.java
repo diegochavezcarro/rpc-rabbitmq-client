ac77f99e2c0ffa5c477711fdfe8d36a0aa213cff
Diego Chavez <diegochavez@diegochavez.local>
2020-03-31 21:54:57 -0300

    jaeger andando

--- src/main/java/com/dchavez/amqp/client/services/RpcClient.java
+++ src/main/java/com/dchavez/amqp/client/services/RpcClient.java
@@ -3,11 +3,13 @@ package com.dchavez.amqp.client.services;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 @Service
+@EnableScheduling
 public class RpcClient {
 	@Autowired
 	private RabbitTemplate template;
